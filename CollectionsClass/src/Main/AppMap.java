bdb032920065210ffcf695b9750d5c7e6b01d3f6
Gustavo Luiz <glogustavo4321@gmail.com>
2022-10-05 19:13:29 -0300

    CollecationMap

--- CollectionsClass/src/Main/AppMap.java
+++ CollectionsClass/src/Main/AppMap.java
@@ -12,11 +12,30 @@ public class AppMap {
 		carteira.put("PETR4", 22.0);
 		carteira.put("MGLU3", 2.20);
 		
+		
 		System.out.println(carteira.get("MGLU3"));
 		
 		for (String key : carteira.keySet()) {
 			System.out.println(key + " : " + carteira.get(key));
 		}
+		
+		//map trabalha atraves de chave
+		
+		Map<String, String> map = new HashMap<>();
+        map.put("Name", "Aman");
+        map.put("Address", "Kolkata");
+  
+        // Print the map
+        System.out.println("Map: " + map);
+  
+        // remap the values using compute() method
+        map.compute("Name", (key, val)
+                                -> val.concat(" Singh"));
+        map.compute("Address", (key, val)
+                                   -> val.concat(" West-Bengal"));
+  
+        // print new mapping
+        System.out.println("New Map: " + map);
 	}
 
 }
